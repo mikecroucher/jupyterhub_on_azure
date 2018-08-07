f5de2c8e2714e04c0d2cdf576040e9aaf7eb0e9d
Mike Croucher <mikecroucher@users.noreply.github.com>
2018-08-07 17:04:40 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,6 @@ sudo apt-get install python3-pip
 #Install Jupyterhub
 sudo npm install -g configurable-http-proxy
 python3 -m pip install jupyterhub
+
+#Installing by pip didn't add the jupyterhub binary folder to the path so let's do that now
+export PATH=$PATH:~/.local/bin
