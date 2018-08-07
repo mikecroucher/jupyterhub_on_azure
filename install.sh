3b5f2e3363b8818130bcad6cc63baee56747f442
Mike Croucher <mikecroucher@users.noreply.github.com>
2018-08-07 16:56:37 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+#Install pre-reqs
 sudo apt-get install npm
+sudo apt-get install python3-pip
+
+#Install Jupyterhub
+sudo npm install -g configurable-http-proxy
+python3 -m pip install jupyterhub
