5a98468ba9e75b764a2499f221844e0413e57c4e
Mike Croucher <michael.p.croucher@googlemail.com>
2018-09-19 18:05:11 +0100

    Report JupyterHub IP address on succesful creation

--- create_vm.sh
+++ create_vm.sh
@@ -3,7 +3,7 @@ keyvaultName=jupyterkeyvault
 location=westus2
 certificateName=mycert
 resourceGroupName=myResourceGroupSecureWeb2
-vmName=jupyter2
+vmName=jupyter9
 
 #Obtain the ID of the certificate we want to use from the keyvault within the VM
 secret=$(az keyvault secret list-versions \
@@ -18,8 +18,14 @@ az vm create \
     --name $vmName \
     --image UbuntuLTS \
     --admin-username azureuser \
+    --custom-data cloud-init.txt \
     --generate-ssh-keys \
     --secrets "$vm_secret"
 
 #Open the port for Jupyter
 az vm open-port --resource-group $resourceGroupName --name $vmName --port 8000
+
+jupyter_ip=$(az vm list --show-details --query "[?name=='$vmName'].{IP:publicIps}" -o tsv)
+
+echo "Jupyterhub should be running at https://$jupyter_ip:8000"
+echo "Give it a minute to start up before you try to access it"
