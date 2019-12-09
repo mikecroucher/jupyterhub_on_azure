6877abf9a0249a43474764d997e27b8bcbbcb2b6
Mike Croucher <mikecroucher@users.noreply.github.com>
2019-12-09 15:30:36 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,10 +23,8 @@ python -m pip install --extra-index-url https://www.nag.com/downloads/py/naginte
 /opt/conda/bin/conda deactivate
 
 #NAG License set up.  Will need a license manually adding in at /opt/nag.lic
-sudo touch /opt/nag.lic
-sudo cat << EOF >> /etc/profile
-export NAG_KUSARI_FILE=/opt/nag.lic
-EOF
+mkdir -p /opt/NAG/
+sudo touch /opt/NAG/nag.key
 
 # Install JupyterHub
 sudo  /opt/conda/bin/conda install jupyterhub -y
@@ -97,7 +95,6 @@ sudo systemctl enable jupyterhub
 #start the service now
 sudo systemctl start jupyterhub
 
-
 #By default, users can read the files in each other's home directory.
 #Change this so only sudo users have this ability
 sudo sed -i s/DIR_MODE=0755/DIR_MODE=0750/g /etc/adduser.conf
@@ -135,90 +132,6 @@ sudo apt-get -y install apg
  userpassword=`apg -n 1`
  echo instructor:$userpassword | sudo chpasswd
  echo "UserID:" instructor "has been created with the following password " $userpassword >> $password_file
- 
-#Install pre-reqs for jupyterhub
-#sudo apt-get -y install npm
-#sudo apt-get -y install python3-pip
-#sudo apt-get -y install nodejs-legacy
-#Update node
-#sudo npm cache clean -f 
-#sudo npm install -g n 
-#sudo n stable
-#install configurable-http-proxy
-#sudo npm install -g configurable-http-proxy
-
-#Install Jupyterhub
-#python3 -m pip install jupyterhub
-#sudo python3 -m pip install sudospawner
-
-#Install the notebook
-#sudo python3 -m pip install notebook
-
-#Installing by pip didn't add the jupyterhub binary folder to the path so let's do that now
-#export PATH=$PATH:~/.local/bin
-
-#Generate default config
-#jupyterhub --generate-config
-
-#Move certificate files
-#secretsname=$(sudo find /var/lib/waagent/ -name "*.prv" | cut -c -57)
-#sudo mkdir -p /etc/jupyter/ssl
-#sudo cp $secretsname.crt /etc/jupyter/ssl/mycert.cert
-#sudo cp $secretsname.prv /etc/jupyter/ssl/mycert.prv
-
-#Make certificate files readable by the user under which we will run the jupyterhub service
-#sudo chgrp azureuser /etc/jupyter/ssl/mycert.cert
-#sudo chgrp azureuser /etc/jupyter/ssl/mycert.prv
-#sudo chmod g+r /etc/jupyter/ssl/mycert.cert
-#sudo chmod g+r /etc/jupyter/ssl/mycert.prv
-
-#Configure certificate in Jupyterhub
-#cat << EOF >> ./jupyterhub_config.py
-#c.JupyterHub.ssl_key = '/etc/jupyter/ssl/mycert.prv'
-#c.JupyterHub.ssl_cert = '/etc/jupyter/ssl/mycert.cert'
-#EOF
-
-#Run on port 443 so that it uses https
-#cat << EOF >> ./jupyterhub_config.py
-#c.JupyterHub.port = 443
-#EOF
-
-#Redirect http:// to https://
-#cat << EOF >> ./jupyterhub_config.py
-#c.ConfigurableHTTPProxy.command = ['configurable-http-proxy', '--redirect-port', '80']
-#EOF
-
-#Add jupyterhub admin users
-#cat << EOF >> ./jupyterhub_config.py
-#c.Authenticator.admin_users = {'azureuser', 'instructor'}
-#EOF
-
-#copy the Jupyterhub config file
-#sudo mkdir -p /etc/jupyterhub
-#sudo cp ./jupyterhub_config.py /etc/jupyterhub/jupyterhub_config.py
-#sudo chown -R azureuser:azureuser /etc/jupyterhub/
-
-#Set up sudospawner
-#Following docs at https://github.com/jupyterhub/jupyterhub/wiki/Using-sudo-to-run-JupyterHub-without-root-privileges retrieved 19th September 2018
-#echo "Cmnd_Alias JUPYTER_CMD=/usr/local/bin/sudospawner" | sudo tee -a /etc/sudoers
-#echo "%jupyterhub ALL=(azureuser) /usr/bin/sudo" | sudo tee -a /etc/sudoers
-#echo "azureuser ALL=(%jupyterhub) NOPASSWD:JUPYTER_CMD" | sudo tee -a /etc/sudoers
-
-#Set up jupyterhub as a service
-#sudo cp ./jupyterhub.service /etc/systemd/system/jupyterhub.service
-
-#Make our user part of the shadow group so that PAM authentication works
-#sudo usermod -a -G shadow azureuser
-
-#Do this next line or we'll not be able to connect to port 443
-#Details at https://github.com/jupyterhub/jupyterhub/issues/774
-#sudo setcap 'cap_net_bind_service=+ep' `which nodejs`
-#sudo setcap 'cap_net_bind_service=+ep' `which node`
-
-#Enable the jupyterhub service so it starts at boot
-#sudo systemctl enable jupyterhub
-#start the service now
-#sudo systemctl start jupyterhub
 
 #Connect the data drive
 #sudo mkdir /datadrive
