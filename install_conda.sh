95756510d1ef7e6e5808509ca92b3be56269f7a2
Mike Croucher <mikecroucher@users.noreply.github.com>
2019-12-10 19:36:12 +0100

    Create install_conda.sh

---
+++ install_conda.sh
@@ -0,0 +1,5 @@
+#Install Conda environment
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x ./Miniconda3-latest-Linux-x86_64.sh
+sudo ./Miniconda3-latest-Linux-x86_64.sh -b -p /opt/conda/
+sudo /opt/conda/bin/conda init --system
