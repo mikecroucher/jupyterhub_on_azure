5af81e2ef64d063beebd6076255bf1a9216fe658
Mike Croucher <michael.p.croucher@googlemail.com>
2019-12-10 20:09:29 +0000

    Fix basic numpy

--- install_multiblas.sh
+++ install_multiblas.sh
@@ -2,11 +2,12 @@
 # Build numpy from source with no optimised BLAS and LAPCK
 apt install gcc -y
 
-conda create -n BLAS-unoptimised Python=3.6 cython ipykernel
 source /opt/conda/etc/profile.d/conda.sh
+conda create -n BLAS-unoptimised Python=3.6 cython ipykernel -y
 conda activate BLAS-unoptimised
 wget https://github.com/numpy/numpy/archive/v1.17.4.tar.gz
 tar -xzf ./v1.17.4.tar.gz
 cd numpy-1.17.4/
+BLAS=None LAPACK=None ATLAS=None python setup.py install
 conda deactivate
 echo "Multiblas script finished"
